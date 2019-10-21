79242f0bb6b1f6dc41cbcf2084936e91d77966a6
thekevingibbons <thekevingibbons@gmail.com>
2019-10-20 20:49:23 -0500

    Re-base
    Changes to allow Jit compatibility, add dictionary support, make PySmores-friendly

--- src/marshmallow/__init__.py
+++ src/marshmallow/__init__.py
@@ -1,34 +1,30 @@
-from marshmallow.schema import Schema, SchemaOpts
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+#Added SchemaJit for toastedmarshmallow compatibility
+from marshmallow.schema import Schema, SchemaOpts, SchemaJit
 
 from . import fields
 from marshmallow.decorators import (
-    pre_dump,
-    post_dump,
-    pre_load,
-    post_load,
-    validates,
-    validates_schema,
+    pre_dump, post_dump, pre_load, post_load, validates, validates_schema
 )
-from marshmallow.utils import EXCLUDE, INCLUDE, RAISE, pprint, missing
+from marshmallow.utils import pprint, missing
 from marshmallow.exceptions import ValidationError
-from distutils.version import LooseVersion
 
-__version__ = "3.2.1"
-__version_info__ = tuple(LooseVersion(__version__).version)
+__version__ = '3.0.0b11.ktg1'
+__author__ = 'Steven Loria, Kevin Gibbons'
 __all__ = [
-    "EXCLUDE",
-    "INCLUDE",
-    "RAISE",
-    "Schema",
-    "SchemaOpts",
-    "fields",
-    "validates",
-    "validates_schema",
-    "pre_dump",
-    "post_dump",
-    "pre_load",
-    "post_load",
-    "pprint",
-    "ValidationError",
-    "missing",
+    'Schema',
+    'SchemaJit', # Added for toastedmarshmallow compatibility
+    'SchemaOpts',
+    'fields',
+    'validates',
+    'validates_schema',
+    'pre_dump',
+    'post_dump',
+    'pre_load',
+    'post_load',
+    'pprint',
+    'ValidationError',
+    'missing',
 ]
